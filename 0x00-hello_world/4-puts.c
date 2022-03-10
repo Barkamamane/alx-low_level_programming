3cf354e304bd944ac23418555c23188351b60c7a
Barkamamane <ibbatambari@gmail.com>
2022-03-10 00:55:32 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  Description: main - prints out a string to sdout.
+ *  Returns: 0 if succes.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzel");
+return (0);
+}
